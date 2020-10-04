c01be60f7fdb15b64c8a7e33c29dc4705ddc2e2a
harika penukonda <penukondaharika@gmail.com>
2020-10-04 12:15:48 -0500

    adding end points to check dev tools

--- src/main/java/com/penukondah1/springboot/mydemoapp/rest/FunRestController.java
+++ src/main/java/com/penukondah1/springboot/mydemoapp/rest/FunRestController.java
@@ -14,5 +14,17 @@ public class FunRestController {
 		return "Hello World! Time on server is " + LocalDateTime.now();
 	}
 	
+	// expose a new end point for "workout"
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run a hard 5K!";
+	}
+	
+	// expose a new end point for "fortune"
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Today is your lucky day";
+	}
+	
 
 }
